274dc477c272bd9ae700e9423409d4b695c0e9a9
akexorcist <akexorcist@gmail.com>
2014-09-11 04:18:36 +0700

    ...

---
+++ src/app/akexorcist/custombutton/Main.java
@@ -0,0 +1,11 @@
+package app.akexorcist.custombutton;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Main extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
